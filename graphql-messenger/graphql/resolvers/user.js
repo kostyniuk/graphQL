c2fa524e2efe9a54f83528ed1ca81ebcb7cf7dc5
kostyniuk <kostyniuk7@gmail.com>
2019-12-11 17:03:42 +0200

    createUser implemented

--- graphql-messenger/graphql/resolvers/user.js
+++ graphql-messenger/graphql/resolvers/user.js
@@ -1,4 +1,7 @@
+/* eslint-disable no-useless-catch */
 'use strict';
+
+const bcrypt = require('bcryptjs');
 
 const db = require('../../db/index');
 
@@ -17,14 +20,32 @@ const resBuilder = arrOfRes => {
 const table = 'person';
 
 module.exports = {
+
   getUsers: async () => {
-    const {rows} = await db.query(`SELECT * FROM ${table};`)
-    return resBuilder(rows)
+    try {
+      const {rows} = await db.query(`SELECT * FROM ${table};`)
+      return resBuilder(rows)
+    } catch(err) {
+      throw(err)
+    }
+    
   }, 
 
   getUser: async ({id}) => {
     const {rows} = await db.query(`SELECT * FROM ${table} WHERE id = $1;`, [id])
     return resBuilder(rows)[0]
+  },
+
+  createUser: async (args) => {
+    const {nickname, firstName, lastName, number, email, password} = args.userInput
+    const hashed = await bcrypt.hash(password, 12);
+    const fields = [nickname, firstName, lastName, number, email, hashed];
+    const query = `
+    INSERT INTO ${table} (nickname, firstName, lastName, number, email, password)
+    VALUES($1, $2, $3, $4, $5, $6) RETURNING id ;`
+    const {rows} = await db.query(query, fields)
+    const {id} = rows[0]
+    return `User succesfully created with the id - ${id}`  
   }
 
 }
